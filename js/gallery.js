a3360039a0bbc4be2b94069fc9817c840671da9d
Alexey Voytovsky <al.voytovsky@gmail.com>
2019-09-04 16:29:40 +0300

    'onClick' method implemented.

--- js/gallery.js
+++ js/gallery.js
@@ -8,7 +8,30 @@ var gallery = {
         document.addEventListener('click', this.onClick.bind(this));
     },
     onClick: function(e) {
-        console.log(e.target);
+        if ( e.target.id == 'prev' ) {
+            --this.index;
+
+            if ( this.index < 0 ) {
+                this.index = this.items.length - 1;
+            }
+
+            this.show();
+            
+        } else if ( e.target.id == 'next' ) {
+            ++this.index;
+
+            if ( this.index >= this.items.length ) {
+                this.index = 0;
+            }
+
+            this.show();
+
+        } else if ( e.target.getAttribute('data-id') ) {
+            this.index = parseInt(e.target.getAttribute('data-id')) - 1;
+
+            this.show();
+            
+        }
     },
     show: function() {
         console.log('show');
