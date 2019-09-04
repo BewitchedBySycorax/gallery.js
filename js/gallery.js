8a8981f2358fd636c8151a71c7aaa7153acc625f
Alexey Voytovsky <al.voytovsky@gmail.com>
2019-09-04 16:20:00 +0300

    'gallery' object improved.

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,18 @@
 var gallery = {
     init: function() {
         console.log('init');
-    }
-    
+        this.items = [ 'img/1.jpg', 'img/2.jpg', 'img/3.jpg' ];
+        this.el = document.getElementById('big');
+        this.image = null;
+        this.index = 0;
+        document.addEventListener('click', this.onClick.bind(this));
+    },
+    onClick: function(e) {
+        console.log(e.target);
+    },
+    show: function() {
+        console.log('show');
+    },
 };
 
 window.onload = function() {
